fd273199d22a0b63a56abd38cff675d2e5b206b2
Kathia Berrios <kathiamarce17@gmail.com>
2020-01-29 14:12:46 -0600

    Añadí ecuaciones

--- MIPRIMERPROY/Form1.Designer.cs
+++ MIPRIMERPROY/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.lblnum1 = new System.Windows.Forms.Label();
             this.lblnum2 = new System.Windows.Forms.Label();
             this.lblRespuesta = new System.Windows.Forms.Label();
+            this.GbEcuacion = new System.Windows.Forms.GroupBox();
+            this.optSuma = new System.Windows.Forms.RadioButton();
+            this.optResta = new System.Windows.Forms.RadioButton();
+            this.optMult = new System.Windows.Forms.RadioButton();
+            this.optDiv = new System.Windows.Forms.RadioButton();
+            this.GbEcuacion.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnprocess
@@ -87,11 +93,69 @@
             this.lblRespuesta.TabIndex = 5;
             this.lblRespuesta.Text = "Respuesta: ?";
             // 
+            // GbEcuacion
+            // 
+            this.GbEcuacion.Controls.Add(this.optDiv);
+            this.GbEcuacion.Controls.Add(this.optMult);
+            this.GbEcuacion.Controls.Add(this.optResta);
+            this.GbEcuacion.Controls.Add(this.optSuma);
+            this.GbEcuacion.Location = new System.Drawing.Point(283, 33);
+            this.GbEcuacion.Name = "GbEcuacion";
+            this.GbEcuacion.Size = new System.Drawing.Size(200, 100);
+            this.GbEcuacion.TabIndex = 6;
+            this.GbEcuacion.TabStop = false;
+            this.GbEcuacion.Text = "Ecuaciones";
+            // 
+            // optSuma
+            // 
+            this.optSuma.AutoSize = true;
+            this.optSuma.Location = new System.Drawing.Point(6, 19);
+            this.optSuma.Name = "optSuma";
+            this.optSuma.Size = new System.Drawing.Size(52, 17);
+            this.optSuma.TabIndex = 0;
+            this.optSuma.TabStop = true;
+            this.optSuma.Text = "Suma";
+            this.optSuma.UseVisualStyleBackColor = true;
+            // 
+            // optResta
+            // 
+            this.optResta.AutoSize = true;
+            this.optResta.Location = new System.Drawing.Point(6, 50);
+            this.optResta.Name = "optResta";
+            this.optResta.Size = new System.Drawing.Size(53, 17);
+            this.optResta.TabIndex = 1;
+            this.optResta.TabStop = true;
+            this.optResta.Text = "Resta";
+            this.optResta.UseVisualStyleBackColor = true;
+            // 
+            // optMult
+            // 
+            this.optMult.AutoSize = true;
+            this.optMult.Location = new System.Drawing.Point(94, 19);
+            this.optMult.Name = "optMult";
+            this.optMult.Size = new System.Drawing.Size(89, 17);
+            this.optMult.TabIndex = 2;
+            this.optMult.TabStop = true;
+            this.optMult.Text = "Multiplicacion";
+            this.optMult.UseVisualStyleBackColor = true;
+            // 
+            // optDiv
+            // 
+            this.optDiv.AutoSize = true;
+            this.optDiv.Location = new System.Drawing.Point(94, 52);
+            this.optDiv.Name = "optDiv";
+            this.optDiv.Size = new System.Drawing.Size(62, 17);
+            this.optDiv.TabIndex = 3;
+            this.optDiv.TabStop = true;
+            this.optDiv.Text = "Division";
+            this.optDiv.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 178);
+            this.ClientSize = new System.Drawing.Size(507, 214);
+            this.Controls.Add(this.GbEcuacion);
             this.Controls.Add(this.lblRespuesta);
             this.Controls.Add(this.lblnum2);
             this.Controls.Add(this.lblnum1);
@@ -102,6 +166,8 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.GbEcuacion.ResumeLayout(false);
+            this.GbEcuacion.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +181,11 @@
         private System.Windows.Forms.Label lblnum1;
         private System.Windows.Forms.Label lblnum2;
         private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.GroupBox GbEcuacion;
+        private System.Windows.Forms.RadioButton optDiv;
+        private System.Windows.Forms.RadioButton optMult;
+        private System.Windows.Forms.RadioButton optResta;
+        private System.Windows.Forms.RadioButton optSuma;
     }
 }
 
